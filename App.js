4ef0cf748ae824ab9c8e27f8de2cd3b0dffa9284
Mathissou <mathissou01@gmail.com>
2024-03-07 14:50:07 +0100

    dsdsds

--- App.js
+++ App.js
@@ -1,13 +1,51 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import FillingRate from './components/FillingRate';
-import TaskList from './components/TaskList';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Constants from "expo-constants";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+import Home from "./screens/Home";
+import Scan from "./screens/Scan";
+import Profile from "./screens/Profile";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <FillingRate />
-      <TaskList />
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="home" size={size} color={color} />
+              ),
+            }}
+          ></Tab.Screen>
+          <Tab.Screen
+            name="About"
+            component={Scan}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="scan" size={size} color={color} />
+              ),
+            }}
+          ></Tab.Screen>
+          <Tab.Screen
+            name="Profile"
+            component={Profile}
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="person" size={size} color={color} />
+              ),
+            }}
+          ></Tab.Screen>
+        </Tab.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
@@ -15,6 +53,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 50,
+    marginTop: Constants.statusBarHeight,
   },
 });
---
+++ screens/Profile.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import FillingRate from "../components/FillingRate";
+import TaskList from "../components/TaskList";
+
+export default function Profile() {
+  return (
+    <View style={styles.container}>
+      <FillingRate />
+      <TaskList />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 50,
+  },
+});
